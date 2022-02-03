495b3361246ab8f16b745523edca5abe3e2da204
cristianarm <65576994+cristianarm@users.noreply.github.com>
2022-02-03 15:54:02 -0500

    Update stress_test.py
    Exception handling
    informational messages

--- stress_test.py
+++ stress_test.py
@@ -7,16 +7,30 @@ from multiprocessing import cpu_count
 import time
 import os
 
+STR_MIN = 10
+
+def load_time():
+    try:
+        set_time = os.environ['STRESS_MINS']
+    except KeyError: 
+        set_time=10
+        print('STRESS_MINS: Environment variable does not exist')
+        print('SET variable  STRESS_MINS=10')
+
 def f(x):
-    set_time = os.environ['STRESS_MINS']
-    timeout = time.time() + 60*float(set_time)  # X minutes from now
+    print('STRESS processor : ', x)
+    timeout = time.time() + 60*float(STR_MIN)  # X minutes from now
     while True:
         if time.time() > timeout:
             break
         x*x
 
 if __name__ == '__main__':
+    load_time()
     processes = cpu_count()
     print ('utilizing %d cores\n' % processes)
     pool = Pool(processes)
-    pool.map(f, range(processes))
+    try:
+        pool.map(f, range(processes))
+    except KeyboardInterrupt: 
+        print("KeyboardInterrupt")
